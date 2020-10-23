b1bd8e5bb3131ce2c583537f34f47844d105abd4
Rafael Zasas <zasas.rafi@gmail.com>
2020-10-22 21:31:00 -0700

    Added Loading Spinner
    dependencies:
    ➕flutter_spinkit: ^4.1.2+1
    
    Additions: 
    Changed main to Stateful widget ♻️
    Added Funky Loading Spinner ✨

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var isLoading = false;
+  var btnText = ['Press me to Load', 'Press me to stop Loading'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +22,34 @@ class MyApp extends StatelessWidget {
           title: Center(child: Text('App 1')),
           backgroundColor: Colors.green,
         ),
-        body: Center(
-            child: Text(
-          'Andrew Yip',
-          style: TextStyle(
-            color: Colors.blue,
-            fontSize: 50.2,
-            fontWeight: FontWeight.bold,
-          ),
+        body: Scaffold(
+            body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Andrew Yip',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 50.2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Text(isLoading ? btnText[1] : btnText[0]), // ternary. if loading true text = [1] else [0]
+                onPressed: () {
+                  setState(() {
+                    // set state method only available to stateful widgets
+                    isLoading = !isLoading; // flip value of isLoading
+                  });
+                },
+              ),
+            ),
+            isLoading ? SpinKitCubeGrid(size: 100.0, color: Colors.lightBlue) : Container() // show spinner if loading
+          ],
         )),
       ),
     );
