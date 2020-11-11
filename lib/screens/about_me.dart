0aec6c6e36d6f18906d20d4d204d7f7972a7344e
Andrew Yip <andrew.yip22@gmail.com>
2020-11-10 21:37:22 -1000

    added more to about me page, getting familiar with child and children widgets

--- lib/screens/about_me.dart
+++ lib/screens/about_me.dart
@@ -7,14 +7,35 @@ class AboutMe extends StatelessWidget {
       appBar: AppBar(
         title: Text("About Me"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      body:
+        Container(
+          child: ListView(
+              children: <Widget>[
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Go back!'),
+                    ),
+                  ),
+        Container (
+          padding: const EdgeInsets.all(16.0),
+          width: MediaQuery.of(context).size.width*0.8,
+              child: new Column (
+                children: <Widget>[
+                  new Text ("Flutter Application 0 Andrew Yip ", textAlign: TextAlign.left, style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  new Text ("This is my first Flutter application. ", textAlign: TextAlign.left),
+                ],
+              ),
+          ),
+              ],
+          )
         ),
-      ),
     );
   }
 }
